b652f5790f4924542ace3084898a940c3379d9d2
Francois Perrad <francois.perrad@gadz.org>
2009-02-19 11:15:59 +0100

    downcase language

--- Configure.pl
+++ Configure.pl
@@ -6,7 +6,7 @@ use warnings;
 use 5.008;
 
 my $build_dir = '../..';
-my $hll       = 'WMLScript';
+my $hll       = 'wmlscript';
 my $cmd       = qq{$^X -Ilib tools/dev/reconfigure.pl --step=gen::languages --languages=$hll};
 
 print "Running '$cmd' in $build_dir\n";
